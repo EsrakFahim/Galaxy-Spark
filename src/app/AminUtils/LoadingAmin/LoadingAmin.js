e21fdee8e608c2cf0cd62e11f073749050f9a4e3
EsrakFahim <efahim0656@gmail.com>
2024-09-11 02:26:17 +0600

    refactor: Update LoadingAmin component to display a spinner with text

--- src/app/AminUtils/LoadingAmin/LoadingAmin.js
+++ src/app/AminUtils/LoadingAmin/LoadingAmin.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const LoadingAmin = () => {
       return <div>
-            <div className="loadingio-spinner-rolling-1z7z7z7z7z7"></div>
+            <div className="loadingio-spinner-rolling-1z7z7z7z7z7">
+                  hello
+            </div>
       </div>;
 };
 
