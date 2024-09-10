aa68090c867b8beb46556ed191174d83d2772656
EsrakFahim <efahim0656@gmail.com>
2024-09-11 02:31:56 +0600

    refactor: Remove unnecessary text from LoadingAmin component

--- src/app/AminUtils/LoadingAmin/LoadingAmin.js
+++ src/app/AminUtils/LoadingAmin/LoadingAmin.js
@@ -2,9 +2,6 @@ import React from "react";
 
 const LoadingAmin = () => {
       return <div>
-            <div className="loadingio-spinner-rolling-1z7z7z7z7z7">
-                  hello
-            </div>
       </div>;
 };
 
