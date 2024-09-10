82de155c96ad9854b23957b0179383857e7cf07c
EsrakFahim <efahim0656@gmail.com>
2024-09-11 02:05:36 +0600

    refactor: Update LoadingAmin component to display a spinner
    The LoadingAmin component has been updated to display a spinner instead of the text "LoadingAmin". This improves the user experience by providing a visual indication of loading. The spinner is implemented using the "loadingio-spinner-rolling-1z7z7z7z7z7" CSS class.

--- src/app/AminUtils/LoadingAmin/LoadingAmin.js
+++ src/app/AminUtils/LoadingAmin/LoadingAmin.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const LoadingAmin = () => {
-      return <div>LoadingAmin</div>;
+      return <div>
+            <div className="loadingio-spinner-rolling-1z7z7z7z7z7"></div>
+      </div>;
 };
 
 export default LoadingAmin;
